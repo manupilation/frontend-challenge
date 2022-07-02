508dd77181ef0ae4cce282d77141c3b3d5a6be4e
Emanoel Mattos <emanoelmattos13@gmail.com>
2022-07-02 03:25:01 -0300

    feat: create globalStyles

--- wine-app/src/pages/index.tsx
+++ wine-app/src/pages/index.tsx
@@ -4,9 +4,12 @@ import Head from '../infra/Head';
 import Header from '../components/Header'
 import WinesList from '../components/WinesList';
 import SearchForm from '../components/SearchForm';
+import GlobalStyle from '../styles/GlobalStyles';
+
 
 const Home: NextPage = () => (
   <div>
+    <GlobalStyle />
     <Head title="Wine" />
 
     <Header />
