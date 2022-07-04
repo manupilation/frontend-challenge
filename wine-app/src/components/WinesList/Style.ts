ac3d044718747cc7d26032f5f253027e68ddda3d
Emanoel Mattos <emanoelmattos13@gmail.com>
2022-07-03 22:56:27 -0300

    feat: create DivWrapper

---
+++ wine-app/src/components/WinesList/Style.ts
@@ -0,0 +1,11 @@
+import styled from 'styled-components';
+
+const DivWrapper = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+  margin-top: 20px;
+  float: left;
+  width: 70%;
+`;
+
+export default DivWrapper;
